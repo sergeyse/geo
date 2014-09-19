9c9157d9f7443c05f7823d7abd7d261ef84ef419
sergeyse <sergiy@simnet.is>
2014-09-19 22:13:12 +0000

    cleaned ,geolocation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,60 +1,21 @@
 'use strict';
 
 angular.module('ispanApp', [
-        'ngAnimate',
-        'ngCookies',
+
         'ngResource',
-        'ngSanitize',
+
         'ngRoute',
-        'trColorFilter',
-        'firebase',
-        'ui.utils'
+
+        'firebase'
+
 
 
 
     ])
     .config(function ($routeProvider) {
         $routeProvider
-            .when('/', {
-                templateUrl: 'views/main.html',
-                controller: 'MainCtrl'
-            })
-            .when('/search/:orderId', {
-                templateUrl: 'views/searchres.html',
-                controller: 'searchCtrl'
-            })
-            .when('/search', {
-                templateUrl: 'views/searchres.html',
-                controller: 'searchCtrl'
 
 
-            })
-            .when('/fullsearch', {
-                templateUrl: 'views/fullsearchres.html',
-                controller: 'fullSearchCtrl'
-
-
-            }).when('/new/:orderId', {
-                templateUrl: 'views/create.html',
-                controller: 'CreateCTRL'
-            })
-            .when('/addnew', {
-                templateUrl: 'views/addnew.html',
-                controller: 'AddnewCtrl'
-            })
-            .when('/landingMap', {
-                templateUrl: 'views/landingmap.html',
-                controller: 'LandingmapCtrl',
-                resolve: {
-                    "currentUser": ["simpleLogin", function (simpleLogin) {
-                     // here we resolving a promise and injecting a "currentUsen into a controller "
-                         return    simpleLogin.$getCurrentUser();
-                        //   return 42;
-
-                    }]
-                }
-
-            })
             .when('/googleMap', {
                 templateUrl: 'views/googlemap.html',
                 controller: 'GooglemapCtrl',
@@ -92,23 +53,23 @@ angular.module('ispanApp', [
                 templateUrl: 'views/loginmain.html',
                 controller: 'LoginmainCtrl'
             })
+            .when('/',{
+                templateUrl: 'views/loginmain.html',
+                controller: 'LoginmainCtrl'
+
+
+            })
             .otherwise({
-                redirectTo: '/'
+                redirectTo: '/loginmain'
             });
     })
-    .value('fbURL', 'https://incandescent-fire-7211.firebaseio.com/rekkar/')
+
     .value('geoURL', 'https://incandescent-fire-7211.firebaseio.com/map/')
 
-    .factory('Orders', function ($firebase, fbURL, $q) {
-        //return $firebase(new Firebase(fbURL));
-        var ref = $firebase(new Firebase(fbURL));
-        return ref.$asArray();
 
 
-    }
 
 
-)
     .factory('simpleLogin', ["$firebaseSimpleLogin", function ($firebaseSimpleLogin) {
         var ref = new Firebase('https://incandescent-fire-7211.firebaseio.com/');
         console.log("login" + $firebaseSimpleLogin(ref));
--- app/scripts/controllers/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('ispanApp')
-  .controller('MainCtrl',['$scope', function ($scope) {
-    $scope.query = '';
-
-
-
-  }]);
